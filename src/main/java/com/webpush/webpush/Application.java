62657e3f514b2b8d4dc459a86e789f092d7cf522
AniruddhaBag2020 <74548950+AniruddhaBag2020@users.noreply.github.com>
2021-08-14 12:45:14 +0530

    Update Application.java

--- src/main/java/com/webpush/webpush/Application.java
+++ src/main/java/com/webpush/webpush/Application.java
@@ -6,14 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableScheduling
+//@SpringBootApplication
+//@EnableScheduling
 public class Application {
 
   public final static Logger logger = LoggerFactory.getLogger(Application.class);
 
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
+  //public static void main(String[] args) {
+  //  SpringApplication.run(Application.class, args);
+  //}
 
 }
